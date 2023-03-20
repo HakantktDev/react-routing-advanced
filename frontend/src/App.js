20486c3a1b6f67c93a5127f7d735b7400d033c9a
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-20 18:35:18 +0300

    reusing-actions-via-request-methods

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { action as manipulateEventAction } from './components/EventForm';
 import EditEventPage from './pages/EditEvent';
-import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from './pages/EventDetail';
+import ErrorPage from './pages/Error';
+import EventDetailPage, { action as deleteEventAction, loader as eventDetailLoader } from './pages/EventDetail';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/Home';
-import NewEventPage, { action as newEventAction } from './pages/NewEvent';
+import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
-import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
   {
@@ -30,10 +31,10 @@ const router = createBrowserRouter([
             loader: eventDetailLoader,
             children: [
               { index: true, element: <EventDetailPage />, action: deleteEventAction },
-              { path: 'edit', element: <EditEventPage /> },
+              { path: 'edit', element: <EditEventPage />, action: manipulateEventAction },
             ],
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
+          { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
         ],
       },
     ],
