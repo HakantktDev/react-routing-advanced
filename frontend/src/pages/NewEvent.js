c61efb855c5f1777ad8e7bc2532d08cabd77cae5
Hakan Tokat <hakantokatdev@gmail.com>
2023-03-20 18:22:48 +0300

    validating-user-input-with-be

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -22,6 +22,10 @@ export const action = async ({ request, params }) => {
     body: JSON.stringify(eventData),
   });
 
+  if (response.status === 422) {
+    return response;
+  }
+
   if (!response.ok) {
     throw json({ message: 'Could not save event' }, { status: 500 });
   }
